2490484e53627626630d6dd62ed01c3846cba758
Leandro <Leandro@pc-prile>
2014-05-09 21:38:09 -0400

    Foi alterado configuração de banco de dados

--- config/configuracoes.php
+++ config/configuracoes.php
@@ -3,5 +3,5 @@ $dados_bd = array();
 $dados_bd = array(  'host' => '127.0.0.1',
                     'dbname' => 'System',
                     'user' => 'root',
-                    'senha' => '10203040');
+                    'senha' => '');
 $conf_contas = array('qtd_meses_conta_fixa'=>120);
