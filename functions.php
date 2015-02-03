043ce1d29364882027e5d097616a17210737bc0e
ekamoto <ekamoto.leandro@gmail.com>
2015-02-03 17:39:26 -0300

    Separando funções

--- functions.php
+++ functions.php
@@ -1393,37 +1393,6 @@ function criptografar($string) {
     return sha1($string);
 }
 
-function validaAcesso($user, $password) {
-    $pdo = conectar();
-    $dados_retorno = array();
-    $password = criptografar($password);
-    $sql = "SELECT id, group_id, name FROM users WHERE username =? AND password=?";
-    $statemente = $pdo->prepare($sql);
-    $statemente->bindParam(1, $user, PDO::PARAM_STR);
-    $statemente->bindParam(2, $password, PDO::PARAM_STR);
-    $executa = $statemente->execute();
-    if ($executa) {
-        if ($statemente) {
-            foreach ($statemente as $value) {
-                $dados_retorno['id'] = $value['id'];
-                $dados_retorno['group_id'] = $value['group_id'];
-                $dados_retorno['name'] = $value['name'];
-            }
-        }
-    }
-    return $dados_retorno;
-}
-
-function startSession($id_user, $group_id, $name) {
-    if (!isset($_SESSION['id_user'])) {
-        $_SESSION['id_user'] = $id_user;
-        $_SESSION['group_id'] = $group_id;
-        $_SESSION['name'] = $name;
-    } else {
-        die('Falha ao iniciar sessão!');
-    }
-}
-
 function updateSession($id_user, $group_id, $name) {
     $_SESSION['id_user'] = $id_user;
     $_SESSION['group_id'] = $group_id;
