b9b8c1af103d2c4a14e5e567e72431b30c5ce996
Leandro Shindi Ekamoto <ekamoto.leandro@gmail.com>
2015-02-16 11:33:01 -0300

    Alterando configurações

--- config/configuracoes.php
+++ config/configuracoes.php
@@ -1,7 +1,12 @@
 <?php
 $dados_bd = array();
-$dados_bd = array(  'host' => '127.0.0.1',
+$dados_bd = array(  'host' => 'localhost',
                     'dbname' => 'System',
                     'user' => 'root',
-                    'senha' => '');
+                    'senha' => 'analisesistemas');
+$dados_bd_admin = array();
+$dados_bd_admin = array(  'host' => 'localhost',
+                    'dbname' => 'System',
+                    'user' => 'root',
+                    'senha' => 'analisesistemas');
 $conf_contas = array('qtd_meses_conta_fixa'=>120);
